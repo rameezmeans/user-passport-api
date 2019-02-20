39816b00c961b73c9cb9398a0f26c193a1204721
vagrant <vagrant@homestead>
2019-02-20 00:28:15 +0000

    first commit

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/login','ApiController@accessToken');
+Route::group(['middleware' => ['web','auth:api']], function()
+{
+    Route::post('/todo/','ApiController@store');
+    Route::get('/todo/','ApiController@index');
+    Route::get('/todo/{todo}','ApiController@show');
+    Route::put('/todo/{todo}','ApiController@update');
+    Route::delete('/todo/{todo}','ApiController@destroy');
+});
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
